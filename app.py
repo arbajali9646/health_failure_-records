4029d00a93bd624fe078d70b94b7db5e30a76148
Arbajali <Arbajali@users.noreply.huggingface.co>
2025-09-22 10:11:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load your model (if you have trained one)
-# model = joblib.load('model.pkl')
+model = joblib.load('model.pkl')
 
 @app.route('/')
 def home():
